a82b041234640ef14ffc63716340f624b7702656
Sven Stiels <52165058+5v3n-08@users.noreply.github.com>
2021-12-08 20:20:33 +0100

    Initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,18 @@
+import { defineNuxtConfig } from "nuxt3";
+
+// https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
+export default defineNuxtConfig({
+  css: ["vuetify/lib/styles/main.sass"],
+  buildModules: [
+    // https://windicss.org/integrations/nuxt.html
+    "nuxt-windicss",
+    // https://pinia.esm.dev/
+    // "@pinia/nuxt",
+  ],
+  env: {
+    DEBUG: false,
+  },
+  build: {
+    transpile: ["vuetify"],
+  },
+});
