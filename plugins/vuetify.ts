6a350030c70b96a39e3e47da5549e2f16655ab89
Sven Stiels <52165058+5v3n-08@users.noreply.github.com>
2021-12-09 20:57:48 +0100

    chore(plugins): add all vuetify cm

--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -1,17 +1,10 @@
 import { defineNuxtPlugin } from "#app";
 import { createVuetify } from "vuetify";
-import { VApp, VAppBar, VBtn } from "vuetify/components";
-
-// Import everything
-// import * as components from 'vuetify/components'
+import * as components from "vuetify/components";
 
 export default defineNuxtPlugin((nuxtApp) => {
   const vuetify = createVuetify({
-    components: {
-      VApp,
-      VAppBar,
-      VBtn,
-    },
+    components,
   });
   nuxtApp.vueApp.use(vuetify);
 });
