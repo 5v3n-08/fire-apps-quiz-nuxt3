1e7b1645b6ded35b4a8633d16d5b5fa1ba7873f6
Sven Stiels <52165058+5v3n-08@users.noreply.github.com>
2021-12-14 14:49:12 +0100

    chore(page): update login page

--- pages/login.vue
+++ pages/login.vue
@@ -1,16 +1,67 @@
 <template>
   <div class="h-screen w-screen">
     <div class="bg-image" />
-    <div class="h-full flex justify-center align-center">
-      <v-card class="text-center">
-        <v-card-title>Anmeldung</v-card-title>
-        <v-card-text>sas</v-card-text>
+    <div class="h-full flex justify-center align-center p-5">
+      <v-card class="text-center w-sm">
+        <v-card-title class="flex justify-center">Anmeldung</v-card-title>
+        <v-card-subtitle class="flex justify-center">
+          Bitte melde dich mit deinem Account an oder erstelle einen neuen.
+        </v-card-subtitle>
+        <div class="p-4">
+          <v-text-field
+            label="E - Mail"
+            hide-details="auto"
+            v-model="email"
+            :rules="[validateRequired]"
+          />
+          <v-text-field
+            label="Passwort"
+            hide-details="auto"
+            v-model="password"
+            :rules="[validateRequired]"
+            :type="showPassword ? 'text' : 'password'"
+          >
+            <template v-slot:append>
+              <v-icon
+                :icon="!showPassword ? 'fas fa-eye' : 'fas fa-eye-slash'"
+                @click="showPassword = !showPassword"
+              ></v-icon>
+            </template>
+          </v-text-field>
+
+          <v-btn color="primary" @click="login">Anmelden</v-btn>
+        </div>
       </v-card>
     </div>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+// TODO: Remove empty space between VTextField by adding 'hide-details="auto"' not working.
+// TODO: Remove password append icon slot with ':append-icon' and '@click:append'.
+import {
+  ERequestAuthenticationMode,
+  useAuthentication,
+} from "@/stores/authentication";
+import { validateEmail, validateRequired } from "~~/helpers/validation";
+
+const email = useState<string>("email", () => "");
+const password = useState<string>("password", () => "");
+const showPassword = useState<boolean>("showPassword", () => false);
+const isLoading = useState<boolean>("isLoading", () => false);
+
+const authenticationStore = useAuthentication();
+const login = async () => {
+  if (validateRequired(email.value) && validateEmail(password.value)) {
+    isLoading.value = true;
+    await authenticationStore.requestAuthentication(
+      ERequestAuthenticationMode.login,
+      { email: email.value, password: password.value }
+    );
+    isLoading.value = false;
+  }
+};
+</script>
 
 <style scoped>
 .bg-image {
